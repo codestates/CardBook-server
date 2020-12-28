8b7472aff78916bdc4585cf0a1704d70aae65045
Ukhwan <dladnrghks22@gmail.com>
2020-12-28 16:14:43 +0900

    update

--- controllers/users/signIn.js
+++ controllers/users/signIn.js
@@ -7,14 +7,8 @@ module.exports ={
         })
         .then(response=>{
             req.session.userid = response.data.id
-<<<<<<< HEAD
             res.status(200).send();
         })
         .catch(err=>console.log(err));
-=======
-            res.status(200).send("<h1>로그인 실행</h1>");
-        })
-        .catch(err=>console.log(err));        
->>>>>>> c38a5afb0c23cc2ae1905f9a9fb8388fbca3bf70
     }
 };
