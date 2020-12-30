07194ef082e51c93209e68f9195d1af794d47e9e
Ukhwan <dladnrghks22@gmail.com>
2020-12-30 10:19:54 +0900

    update some codes

--- controllers/contents/viewPublicLists.js
+++ controllers/contents/viewPublicLists.js
@@ -4,5 +4,6 @@ module.exports ={
     get: async (req,res)=>{
         await contents.findAll()
         .then(data=>res.send(200,data))
+        .catch(()=>res.send(404,"This user is invalid"));
     }
 }
